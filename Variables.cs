72cbe4b87a99b324c58dba38f05f1ab13a425cc3
chankxow <101338054+chankxow@users.noreply.github.com>
2023-05-09 20:52:28 +0700

    Create Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // type variableName = value;
+
+            // STRING
+            string name = "KIATTIPOOM";
+            Console.WriteLine(name);
+
+            // INT & DOUBLE
+            int varNum;
+            double varDub = 5.5;
+            varNum = 0;
+            int myNum = 18;
+            Console.WriteLine(myNum);
+            Console.WriteLine(varNum);
+            Console.WriteLine(varDub);
+
+            // BOOL
+
+            bool myBool = false;
+            Console.WriteLine(myBool);
+
+            // EXAMPLE
+
+            int Number = 7855;
+            double myDouble = 59.555;
+            char letter = 'H';
+            bool theTrue = true;
+            string Text = "Hello, my nickname is Poom";
+
+            Console.WriteLine(Number);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(letter);
+            Console.WriteLine(theTrue);
+            Console.WriteLine(Text);
+
+        }  
+    }      
+}
