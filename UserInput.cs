25e41bbbe9292d5e00ba1c533e4bdda2889b760e
chankxow <101338054+chankxow@users.noreply.github.com>
2023-05-10 10:55:44 +0700

    XD

---
+++ UserInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER YOUR NAME : ");
+            String Name = Console.ReadLine();
+            Console.WriteLine("ENTER YOUR AGE :");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name is  " + Name);
+            Console.WriteLine("Your age is " + Age);
+
+        }
+    }
+}
