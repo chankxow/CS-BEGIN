1e7b89b72aa673498d4d70ac2eb3490c601c04c2
chankxow <101338054+chankxow@users.noreply.github.com>
2023-05-10 10:57:31 +0700

    Create operators.cs

---
+++ operators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 100 +  50; //100 (100+50)
+            int sum2 = sum1 + 250; //400 (150+250)
+            int sum3 = sum2 + sum2;//800 (400+400)
+
+            Console.WriteLine(sum3);
+
+        }
+    }
+}
