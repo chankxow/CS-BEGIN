5d89ed1c23a888c906a12649b311defac8ac0f2c
chankxow <101338054+chankxow@users.noreply.github.com>
2023-05-11 13:06:01 +0700

    Create ifelse.cs

---
+++ ifelse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+
+            int time = 21;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+            // Outputs "Good evening."
+
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+            // Outputs "Good evening."
+
+        }
+    }
+}
