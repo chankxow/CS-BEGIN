e20f8b936ef3b7b8dc0c9fe92c6c5ea20b154e83
chankxow <101338054+chankxow@users.noreply.github.com>
2023-05-11 12:45:51 +0700

    Create booleans.cs

---
+++ booleans.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booleans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 20;
+            Console.WriteLine(x > y); // outputs false
+
+            Console.WriteLine(20>10);// outputs true
+
+            int c = 10;
+            Console.WriteLine(c == 10); // true
+
+            int myAge = 25;
+            int votingAge = 18;
+            if(myAge >= votingAge) 
+            {
+                Console.WriteLine("Old enough to vote!");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough to vote.");
+            }
+
+            
+        }
+    }
+}
